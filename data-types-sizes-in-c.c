af69c0e85084285e4b09d79b8243e79d046ce990
Aditya Pawar <fycs.adityapawar.18332@gmail.com>
2023-08-17 00:07:46 +0530

    Completed printf statement, variables intro and data types size lessons.

---
+++ data-types-sizes-in-c.c
@@ -0,0 +1,15 @@
+// PROGRAM THAT WILL PRINT THE SIZES OF DATA TYPES IN C.
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("a char is %ld bytes\n", sizeof(char));
+    printf("an int is %ld bytes\n", sizeof(int));
+    printf("an float is %ld bytes\n", sizeof(float));
+    printf("a double is %ld bytes\n", sizeof(double));
+    printf("a short int is %ld bytes\n", sizeof(short int));
+    printf("a long int is %ld bytes\n", sizeof(long int));
+    printf("a long double is %ld bytes\n", sizeof(long double));
+    return 0;
+}
---
+++ print-in-c.c
@@ -0,0 +1,8 @@
+// PROGRAM TO PRINT SOMEHTING IN TERMINAL/CONSOLE.
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+}
